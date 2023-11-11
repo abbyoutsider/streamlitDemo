0e5fbefbaec3dadbbff4c7d8e2fe0ce178a67da6
Yumeng Wang <abigail.wang.711@gmail.com>
2023-11-11 11:16:03 -0800

    add main function

--- demo.py
+++ demo.py
@@ -23,13 +23,17 @@ def find_N_nearest_words(input, N):
   return [w for s,w in similarities[:N]]
 
 
-st.title("Find The Nearest Image App")
-prev_input=''
-user_input = st.text_input("Enter a word:")
+def main():
+  st.title("Find The Nearest Image App")
+  prev_input=''
+  user_input = st.text_input("Enter a word:")
 
-if prev_input != user_input:
-        prev_input = user_input
-        matched_word = find_N_nearest_words(user_input, 1)[0]
-        st.caption("Find the nearest image: "+matched_word)
-        st.image(mapping.get(matched_word))
+  if prev_input != user_input:
+          prev_input = user_input
+          matched_word = find_N_nearest_words(user_input, 1)[0]
+          st.caption("Find the nearest image: "+matched_word)
+          st.image(mapping.get(matched_word))
 
+
+if __name__ == "__main__":
+  main()
